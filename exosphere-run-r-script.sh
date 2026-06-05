6ab8956896fea66ccbde021282efb22e8346b9bb
Tyler L. McIntosh <tylermcintosh7@gmail.com>
2026-06-05 13:46:30 -0600

    add exosphere test scripts

---
+++ exosphere-run-r-script.sh
@@ -0,0 +1,29 @@
+# If no permissions:
+# chmod +x run_r_script.sh
+
+# Usage
+# ./run_r_script.sh analysis.R
+
+#!/bin/bash
+IMAGE="tylerlmcintosh/rocker_eds:4.6-cran20260501"
+DEV_DIR="/media/volume/test-volume"
+CONTAINER_DEV_DIR="/home/rstudio/dev"
+
+# 1. Check if the user provided a script name argument
+if [ -z "$1" ]; then
+    echo "Error: Please specify the R script you want to run."
+    echo "Usage: ./run_r_script.sh your_script.R"
+    exit 1
+fi
+
+SCRIPT_NAME="$1"
+
+echo "Launching container to run script: $SCRIPT_NAME"
+echo "--------------------------------------------------------"
+
+# 2. Run Docker, map the volume, and execute Rscript
+docker run --rm \
+  -v "${DEV_DIR}:${CONTAINER_DEV_DIR}" \
+  -w $CONTAINER_DEV_DIR \
+  $IMAGE \
+  Rscript "$SCRIPT_NAME"
---
+++ rocker_eds/exosphere-rstudio.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+IMAGE="tylerlmcintosh/rocker_eds:4.6-cran20260501"
+HOST_PORT=8787
+DEV_DIR="/media/volume/test-volume"
+CONTAINER_DEV_DIR="/home/rstudio/dev"
+PASSWORD="123"
+URL="http://localhost:${HOST_PORT}"
+
+# 1. Start the Docker container in the background
+echo "Starting Rocker RStudio container..."
+docker run -d --rm \
+  -p ${HOST_PORT}:8787 \
+  -e PASSWORD=$PASSWORD \
+  -v "${DEV_DIR}:${CONTAINER_DEV_DIR}" \
+  -w $CONTAINER_DEV_DIR \
+  $IMAGE
+
+# 2. Wait 4 seconds for the RStudio server to spin up
+echo "Waiting for container to initialize..."
+sleep 4
+
+# 3. Automatically launch the browser tab inside your Web Desktop
+echo "Opening RStudio at $URL"
+xdg-open "$URL" > /dev/null 2>&1 &
