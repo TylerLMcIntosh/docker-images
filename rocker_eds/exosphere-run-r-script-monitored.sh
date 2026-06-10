3dc3695da4bf74bf65a9bb39ad749e9eda112aa8
Tyler L. McIntosh <tylermcintosh7@gmail.com>
2026-06-10 16:10:07 -0600

    fix for here()

--- rocker_eds/exosphere-run-r-script-monitored.sh
+++ rocker_eds/exosphere-run-r-script-monitored.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# run_r_script_monitored.sh
-# Usage: ./run_r_script_monitored.sh <script.R> [interval_seconds] [monitor_script] [log_dir]
+# exosphere-run-r-script-monitored.sh
+# Usage: ./exosphere-run-r-script-monitored.sh <script.R> [interval_seconds] [monitor_script] [log_dir] [project_subdir]
 #
 # Arguments:
-#   $1  R script to run (relative to DEV_DIR)
+#   $1  R script to run (relative to project_subdir)
 #   $2  Monitor interval in seconds (default: 10)
-#   $3  Path to monitor_resources.sh (relative to DEV_DIR, default: monitor_resources.sh)
-#   $4  Directory for log outputs (relative to DEV_DIR, default: logs)
+#   $3  Path to monitor-resources.sh (relative to DEV_DIR, default: docker-images/rocker_eds/monitor-resources.sh)
+#   $4  Directory for log outputs (relative to DEV_DIR, default: compound-disturbance-resilience/logs)
+#   $5  Project subdirectory within DEV_DIR (default: compound-disturbance-resilience)
 
 IMAGE="tylerlmcintosh/rocker_eds:4.6-cran20260501"
 DEV_DIR="/media/volume/working-volume"
@@ -14,14 +15,15 @@ CONTAINER_DEV_DIR="/home/rstudio/dev"
 
 if [ -z "$1" ]; then
   echo "Error: Please specify the R script you want to run."
-  echo "Usage: ./run_r_script_monitored.sh <script.R> [interval_seconds] [monitor_script] [log_dir]"
+  echo "Usage: ./exosphere-run-r-script-monitored.sh <script.R> [interval_seconds] [monitor_script] [log_dir] [project_subdir]"
   exit 1
 fi
 
 SCRIPT_NAME="$1"
 INTERVAL=${2:-10}
 MONITOR_SCRIPT=${3:-docker-images/rocker_eds/monitor-resources.sh}
-LOG_DIR=${4:-logs}
+LOG_DIR=${4:-compound-disturbance-resilience/logs}
+PROJECT_SUBDIR=${5:-compound-disturbance-resilience}
 
 # derive log filename from script name, place in log dir
 SCRIPT_BASENAME=$(basename "${SCRIPT_NAME%.R}")
@@ -32,25 +34,28 @@ echo "Monitor script:             $MONITOR_SCRIPT"
 echo "Log directory:              $LOG_DIR"
 echo "Resource log:               $LOG_NAME"
 echo "Monitor interval:           ${INTERVAL}s"
+echo "Project subdirectory:       $PROJECT_SUBDIR"
 echo "--------------------------------------------------------"
 
 docker run --rm \
   -e USERID=$(id -u) \
   -e GROUPID=$(id -g) \
   -v "${DEV_DIR}:${CONTAINER_DEV_DIR}" \
-  -w "$CONTAINER_DEV_DIR" \
+  -w "$CONTAINER_DEV_DIR/$PROJECT_SUBDIR" \
   $IMAGE \
   bash -c "
-    # create log directory if it doesn't exist
-    mkdir -p '$LOG_DIR'
+    # create log directory if it doesn't exist (absolute path so it
+    # works regardless of working directory)
+    mkdir -p '$CONTAINER_DEV_DIR/$LOG_DIR'
 
     # launch R in background
     Rscript '$SCRIPT_NAME' &
     R_PID=\$!
     echo \"R process started with PID \$R_PID\"
 
-    # launch monitor in background
-    bash '$MONITOR_SCRIPT' \$R_PID '$LOG_NAME' '$INTERVAL' &
+    # launch monitor in background (absolute paths for both monitor
+    # script and log file so they resolve from any working directory)
+    bash '$CONTAINER_DEV_DIR/$MONITOR_SCRIPT' \$R_PID '$CONTAINER_DEV_DIR/$LOG_NAME' '$INTERVAL' &
     MON_PID=\$!
 
     # wait for R to finish and capture its exit code
